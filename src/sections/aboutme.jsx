a485aa53e2ac11cea1f52d59989c137bc5c375ab
Si_wayril <itsmezyriel@gmail.com>
2025-11-17 20:57:58 +0800

    Update aboutme.jsx

--- src/sections/aboutme.jsx
+++ src/sections/aboutme.jsx
@@ -1,7 +1,13 @@
 import { motion } from "framer-motion";
 import { FaChess, FaBasketballBall, FaBiking, FaGamepad } from "react-icons/fa";
 import { useState, useEffect } from "react";
-const images = ["/assets/aboutme1.jpg", "/assets/aboutme2.jpg", "/assets/aboutme3.jpg", "/assets/aboutme4.jpg"];
+import aboutme1 from "../assets/aboutme1.jpg";
+import aboutme2 from "../assets/aboutme2.jpg";
+import aboutme3 from "../assets/aboutme3.jpg";
+import aboutme4 from "../assets/aboutme4.jpg";
+
+const images = [aboutme1, aboutme2, aboutme3, aboutme4];
+
 
 export default function AboutMe() {
   const [currentImage, setCurrentImage] = useState(0);
